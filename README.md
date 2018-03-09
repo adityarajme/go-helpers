1289d9801fa3436c5bf866bad5cf170122135752
Aditya Raj <adityaraj@Adityas-MacBook.local>
2018-03-09 11:24:12 +0530

    Improvements - Aditya Raj, Intelinno

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # golang_helpers
 Helper functions to speed up Golang based software development.
+
+By: https://www.intelinno.com
--- dates.go
+++ dates.go
@@ -48,24 +48,6 @@ func GetRawDate(dtval string) string {
 	return dtnew.Format("2006-01-02") // at 15:04
 }
 
-func GetChatCodeTimeStamp(timeval string) string {
-	dtnew := LocaliseDate().Format("Jan 2, 2006")
-	return GetTimeStampFromDate(dtnew + " at "+ timeval)
-}
-
-func GetChatCodeTimeStampNextDay(timeval string) string {
-	nextday := LocaliseDate().Add(time.Hour * 24)
-	dtnew := nextday.Format("Jan 2, 2006")
-	return GetTimeStampFromDate(dtnew + " at "+ timeval)
-}
-
-func GetChatCodePrevDayTimeStamp(ts string) string {
-	location, err := time.LoadLocation("America/Los_Angeles")
-	CheckErr2(err)
-	tm, _ := time.Parse("20060102150405",ts)
-	return tm.In(location).Add(time.Hour * -24).Format("20060102150405")
-}
-
 func GetFormattedDateTime(dtval string) string {
 	dtnew := LocaliseTime(dtval)
 	return dtnew.Format("Jan 2, 2006 at 3:04 PM") // at 15:04
@@ -81,7 +63,6 @@ func GetRawFormattedDateTimeFull(dtval string) string {
 	return dtnew.Format("2006-01-02 15:04:05") // at 15:04
 }
 
-//20060102150405
 func LocaliseTime(dtval string) time.Time {
 	location, err := time.LoadLocation("America/Los_Angeles")
 	CheckErr2(err)
